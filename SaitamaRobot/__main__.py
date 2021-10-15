e5083e864bc2098fac02b669ef50ea0f3fec0123
AnshNityam <83384892+YashBebe@users.noreply.github.com>
2021-10-15 12:16:14 +0530

    Ggg

--- SaitamaRobot/__main__.py
+++ SaitamaRobot/__main__.py
@@ -61,7 +61,7 @@ What can i do?
  • I can warn users until they reach max warns, with each predefined actions such as ban, mute, kick, etc.
  • I have a note keeping system, blacklists, and even pre determined replies on certain keywords.
 *Checkout Full Help menu by sending* `/help` *to know about my modules and usage.*
-""",
+"""
 buttons = [
   [
                         InlineKeyboardButton(
