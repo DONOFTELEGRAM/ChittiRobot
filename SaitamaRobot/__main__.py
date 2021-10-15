a10b4f22485bf4ba65c2a84ec99e4aa4f5bf8870
AnshNityam <83384892+YashBebe@users.noreply.github.com>
2021-10-15 11:33:05 +0530

    Maybe fixed..

--- SaitamaRobot/__main__.py
+++ SaitamaRobot/__main__.py
@@ -207,7 +207,7 @@ def start(update: Update, context: CallbackContext):
  InlineKeyboardButton(text="Updates", url="t.me/RajniUpdates")],
 [InlineKeyboardButton(text="Global",url="t.me/RajniGlobal"),
  InlineKeyboardButton(text="Devs", url="t.me/ShajniDevs")],
-[InlineKeyboardButton(text="Help", callback_data="get_help")]))
+[InlineKeyboardButton(text="Help", callback_data="get_help")]])
     else:
         update.effective_message.reply_text(
             "I'm awake already!\n<b>Haven't slept since:</b> <code>{}</code>".format(
