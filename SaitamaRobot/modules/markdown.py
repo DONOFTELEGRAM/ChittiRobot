ab5b50d8818473fef794a6b7ce2984f5864afd2d
AnshNityam <83384892+YashBebe@users.noreply.github.com>
2021-10-17 11:39:14 +0530

    Slight change

--- SaitamaRobot/modules/markdown.py
+++ SaitamaRobot/modules/markdown.py
@@ -2,25 +2,21 @@ import html
 
 __mod_name__ = "Markdown"
 
-__help__ = f"""
+__help__ = """
 Markdown is a very powerful formatting tool supported by telegram. @RajniiRobot has some enhancements, to make sure that \
 saved messages are correctly parsed, and to allow you to create buttons.
-• _italic_: wrapping text with '_' will produce italic text
-• *bold*: wrapping text with '*' will produce bold text
-• `code`: wrapping text with '`' will produce monospaced text, also known as 'code'
-• [sometext](www.url.com): this will create a link - the message will just show ‘sometext’, \
-and tapping on it will open the page at ‘www.url.com’.
-<b>Example:</b>
-`[test](example.com)`
-• `[buttontext](buttonurl:url.com)`: this is a special enhancement to allow users to have telegram \
-buttons in their markdown.
-`buttontext` will be what is displayed on the button, and <code>someurl</code> \
-will be the url which is opened.
-*Example:*
-`[This is a button](buttonurl:example.com)`
-If you want multiple buttons on the same line, use :same, as such:
+• italic: wrapping text with '_' will produce italic text
+• bold: wrapping text with '*' will produce bold text
+• code or monospace: wrapping text with '`' will produce monospaced text, also known as code
+• [sometext](url): this will create a link - the message will just show sometext, \
+and tapping on it will open the page at given url in ().
+
+
+• Inline Button:
+[This is a button](buttonurl:example.com)
+If you want multiple buttons on the same line, use ‘:same’, as such:
 [one](buttonurl://example.com)
 [two](buttonurl://google.com:same)
 This will create two buttons on a single line, instead of one button per line.
-Keep in mind that your message *MUST* contain some text other than just a button!
+Keep in mind that your message *MUST contain some text other than just a button!
 """
