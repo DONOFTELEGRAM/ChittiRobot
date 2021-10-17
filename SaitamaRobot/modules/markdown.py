ed92a32147ca5af03ae1bd58f1f1776c42d781b1
AnshNityam <83384892+YashBebe@users.noreply.github.com>
2021-10-17 12:18:11 +0530

    Update markdown.py

--- SaitamaRobot/modules/markdown.py
+++ SaitamaRobot/modules/markdown.py
@@ -2,7 +2,8 @@ import html
 
 __mod_name__ = "Markdown"
 
-__help__ = f"""Markdown is a very powerful formatting tool supported by telegram. @RajniiRobot has some enhancements, to make sure that 
+__help__ = """
+Markdown is a very powerful formatting tool supported by telegram. @RajniiRobot has some enhancements, to make sure that 
 saved messages are correctly parsed, and to allow you to create buttons.
 • italic: wrapping text with '_' will produce italic text
 • bold: wrapping text with '*' will produce bold text
@@ -11,10 +12,6 @@ saved messages are correctly parsed, and to allow you to create buttons.
 and tapping on it will open the page at given url in (). (Without space between [] & ())
 
 • Inline Button:
-[This is a button] (buttonurl:example.com). (Without space between [] & ())
-If you want multiple buttons on the same line, use ‘:same’ at second button,
-[one] (buttonurl://example.com)
-[two] (buttonurl://google.com:same)
-This will create two buttons on a single line, instead of one button per line.
-Keep in mind that your message MUST contain some text other than just a button!
+Contact @RajniSupport to know about how to make
+ Inline Url button.
 """
