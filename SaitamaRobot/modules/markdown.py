f0353c8d2bc1ce77a60667bb5477d8c08f198a7e
AnshNityam <83384892+YashBebe@users.noreply.github.com>
2021-10-17 13:53:40 +0530

    Update markdown.py

--- SaitamaRobot/modules/markdown.py
+++ SaitamaRobot/modules/markdown.py
@@ -1,17 +1,12 @@
-import html
-
-__mod_name__ = "Markdown"
 
 __help__ = """
 Markdown is a very powerful formatting tool supported by telegram. @RajniiRobot has some enhancements, to make sure that 
 saved messages are correctly parsed, and to allow you to create buttons.
-• italic: wrapping text with '_' will produce italic text
-• bold: wrapping text with '*' will produce bold text
-• code or monospace: wrapping text with '`' will produce monospaced text, also known as code
-• [sometext] (url): this will create a link - the message will just show sometext, 
-and tapping on it will open the page at given url in (). (Without space between [] & ())
-
-• Inline Button:
+• italic: wrapping text with 2 undersore will produce italic text.
+• bold: wrapping text with Asterisk will produce bold text.
+• code or monospace: wrapping text with '`' will produce monospaced text, also known as code.
+• Inline Button & link text:
 Contact @RajniSupport to know about how to make
- Inline Url button.
+ Inline Url button or link text.
 """
+__mod_name__ = "Markdown"
