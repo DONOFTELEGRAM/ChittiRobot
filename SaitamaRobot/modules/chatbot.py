559a1edcb401cb6b1a8ca9cb6c257c0d73f85f24
AnshNityam <83384892+YashBebe@users.noreply.github.com>
2021-10-19 14:36:27 +0530

    Update chatbot.py

--- SaitamaRobot/modules/chatbot.py
+++ SaitamaRobot/modules/chatbot.py
@@ -118,7 +118,7 @@ def chatbot(update: Update, context: CallbackContext):
             return
         Message = message.text
         bot.send_chat_action(chat_id, action="typing")
-        kukiurl = requests.get('http://kukiapi.xyz/api/apikey=key/reply/rajniirobot/@itzzzyashu/female/2/language=en/message='+Message)
+        kukiurl = requests.get('http://kukiapi.xyz/api/apikey=KUKIETVtz816d/reply/rajniirobot/@itzzzyashu/female/2/language=en/message='+Message)
         Kuki = json.loads(kukiurl.text)
         kuki = Kuki['reply']
         sleep(0.3)
