bcd6ad41a4362875d56ff693737a73b33d4033c9
AnshNityam <83384892+YashBebe@users.noreply.github.com>
2021-11-18 23:31:54 +0530

    Updated Help buttons

--- SaitamaRobot/__main__.py
+++ SaitamaRobot/__main__.py
@@ -270,7 +270,7 @@ def start(update: Update, context: CallbackContext):
                     HELPABLE[mod].__help__,
                     InlineKeyboardMarkup(
                         [[InlineKeyboardButton(text="Back", callback_data="help_back"),
-                          InlineKeyboardButton(text="Home", callback_data="start"]]
+                          InlineKeyboardButton(text="Home", callback_data="start")]]
                     ),
                 )
 
