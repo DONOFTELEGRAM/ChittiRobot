07eba6780823d38df8f015fe22b8611aa28dfdf6
AnshNityam <83384892+YashBebe@users.noreply.github.com>
2021-11-20 01:07:31 +0530

    Updated `Ban Event` message.

--- SaitamaRobot/modules/bans.py
+++ SaitamaRobot/modules/bans.py
@@ -110,11 +110,10 @@ def ban(update: Update, context: CallbackContext) -> str:
 
         # bot.send_sticker(chat.id, BAN_STICKER)  # banhammer marie sticker
         reply = (
-            f"<code>❕</code><b>Ban Event</b>\n"
-            f"<code> </code><b>•  User:</b> {mention_html(member.user.id, html.escape(member.user.first_name))}"
+            f"<i>I've banned {mention_html(member.user.id, html.escape(member.user.first_name))} without time limits.</i>"
         )
         if reason:
-            reply += f"\n<code> </code><b>•  Reason:</b> \n{html.escape(reason)}"
+            reply += f"\n• Reason: \n{html.escape(reason)}"
         bot.sendMessage(chat.id, reply, parse_mode=ParseMode.HTML, quote=False)
         return log
 
