8a71b43aa477c3f0d5bdf8be1be96b6f406143f8
itzzzyashu <83384892+itzzzzyashu@users.noreply.github.com>
2021-12-05 23:36:01 +0530

    Update __main__.py

--- SaitamaRobot/__main__.py
+++ SaitamaRobot/__main__.py
@@ -412,7 +412,7 @@ def admin_setup(update, context):
                  \n• To avoid slowing down, Rajnii caches admin rights for each user. This cache lasts about 10 minutes; this may change in the future. This means that if you promote a user manually (without using the /promote command), Rajnii will only find out ~10 minutes later.
                  \n• If you want to update them immediately, you can use the `/admincache` command,thta'll force Rajnii to check who the admins are again and their permissions
                  \n• If you are getting a message saying:
-                     `You must be this chat administrator to perform this action!`
+`You must be this chat administrator to perform this action!`
                  \n• This has nothing to do with Rajnii’s rights; this is all about your permissions as an admin. Rajnii respects admin permissions; if you do not have the Ban Users permission as a telegram admin, you won't be able to ban users with Rajnii. Similarly, to change Rajnii settings, you need to have the Change group info permission.
                  \n• The message very clearly says that you need these admin rights; Rajnii already have.""",
             parse_mode=ParseMode.MARKDOWN,
@@ -433,37 +433,37 @@ def antispam_setup(update, context):
             text="""*｢ Anti-Spam Setup 」*
                  \n\n\n*« Anti-Spam »*
                  \n• `/antispam <on/off/yes/no>`: Change antispam security settings in the group, or return your current settings(when no arguments).
-                 This helps protect you and your groups by removing spam flooders as quickly as possible.
+This helps protect you and your groups by removing spam flooders as quickly as possible.
 
                  \n\n*« Anti-Flood »*
                  \n• `/setflood <int/'no'/'off'>`: enables or disables flood control
                  \n• `/setfloodmode <ban/kick/mute/tban/tmute> <value>`: Action to perform when user have exceeded flood limit. ban/kick/mute/tmute/tban
-                   Antiflood allows you to take action on users that send more than x messages in a row. Exceeding the set flood will result in restricting that user.
+Antiflood allows you to take action on users that send more than x messages in a row. Exceeding the set flood will result in restricting that user.
 
                  \n\n*« Blacklist »*
                  \n• `/addblacklist <triggers>`: Add a trigger to the blacklist. Each line is considered one trigger, so using different lines will allow you to add multiple triggers.
                  \n• `/blacklistmode <off/del/warn/ban/kick/mute/tban/tmute>`: Action to perform when someone sends blacklisted words.
-                   Blacklists are used to stop certain triggers from being said in a group. Any time the trigger is mentioned, the message will immediately be deleted. A good combo is sometimes to pair this up with warn filters!
+Blacklists are used to stop certain triggers from being said in a group. Any time the trigger is mentioned, the message will immediately be deleted. A good combo is sometimes to pair this up with warn filters!
 
                  \n\n*« Reports »*
                  \n• `/reports <on/off>`: Change report setting, or view current status.
                  \n  × If done in pm, toggles your status.
                  \n  × If in chat, toggles that chat's status.
-                 If someone in your group thinks someone needs reporting, they now have an easy way to call all admins.
+If someone in your group thinks someone needs reporting, they now have an easy way to call all admins.
 
                  \n\n*« Locks »*
                  \n• `/lock <type>`: Lock items of a certain type (not available in private)
                  \n• `/locktypes`: Lists all possible locktypes
-                 The locks module allows you to lock away some common items in the telegram world; the bot will automatically delete them!
+The locks module allows you to lock away some common items in the telegram world; the bot will automatically delete them!
 
                  \n\n*« Warns »*
                  \n• `/addwarn <keyword> <reply message>`: Sets a warning filter on a certain keyword. If you want your keyword to be a sentence, encompass it with quotes, as such: /addwarn "very angry" This is an angry user. 
                  \n• `/warn <userhandle>`: Warns a user. After 3 warns, the user will be banned from the group. Can also be used as a reply.
                  \n• `/strongwarn <on/yes/off/no>`: If set to on, exceeding the warn limit will result in a ban. Else, will just kick.
-                   If you're looking for a way to automatically warn users when they say certain things, use the /addwarn command.
+If you're looking for a way to automatically warn users when they say certain things, use the /addwarn command.
                  \n\n*« Captcha »*
                  \n• `/captcha <off/soft/strong>`: All users that join, get muted
-                   A button gets added to the welcome message for them to unmute themselves. This proves they aren't a bot! soft - restricts users ability to post media for 24 hours. strong - mutes on join until they prove they're not bots.""",
+A button gets added to the welcome message for them to unmute themselves. This proves they aren't a bot! soft - restricts users ability to post media for 24 hours. strong - mutes on join until they prove they're not bots.""",
             parse_mode=ParseMode.MARKDOWN,
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(
