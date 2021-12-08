7d296fc559a7a3d9253f5fb2f8bcfea640560a60
itzzzyashu <83384892+itzzzzyashu@users.noreply.github.com>
2021-12-09 03:11:03 +0530

    Ok

--- SaitamaRobot/modules/mongo/nsfw_mongo.py
+++ SaitamaRobot/modules/mongo/nsfw_mongo.py
@@ -26,7 +26,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
 
-from Cutiepii_Robot import db
+from SaitamaRobot import db
 from typing import Dict, List, Union
 
 
