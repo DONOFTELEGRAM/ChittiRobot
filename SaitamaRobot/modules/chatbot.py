f90fce8826f1e3e995ec5da7e97d5f9b1d3f88e1
itzzzyashu <83384892+itzzzzyashu@users.noreply.github.com>
2021-12-22 21:51:00 +0530

    Update chatbot.py

--- SaitamaRobot/modules/chatbot.py
+++ SaitamaRobot/modules/chatbot.py
@@ -118,7 +118,7 @@ def chatbot(update: Update, context: CallbackContext):
             return
         Message = message.text
         bot.send_chat_action(chat_id, action="typing")
-        kukiurl = requests.get('https://www.kukiapi.xyz/api/apikey=KUKIETVtz816d/Rajnii/@Itzzzyashu/message='+Message)
+        kukiurl = requests.get('http://kukiapi.xyz/api/message='+Message)
         Kuki = json.loads(kukiurl.text)
         kuki = Kuki['reply']
         sleep(0.3)
