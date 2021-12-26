f60f3761cd528dd7c9ee20b8b7c669c41bd21523
itzzzyashu <83384892+itzzzzyashu@users.noreply.github.com>
2021-12-26 23:49:09 +0530

    Ok

--- SaitamaRobot/__init__.py
+++ SaitamaRobot/__init__.py
@@ -86,7 +86,7 @@ if ENV:
     CERT_PATH = os.environ.get("CERT_PATH")
     API_ID = os.environ.get("API_ID", None)
     API_HASH = os.environ.get("API_HASH", None)
-    DB_URI = os.environ.get("DATABASE_URL")
+    DB_URL = os.environ.get("DATABASE_URL")
     MONGO_URI = os.environ.get("MONGO_DB_URI")
     MONGO_DB = "Rajnii"
 #    STRING_SESSION = os.environ.get("STRING_SESSION", None)
