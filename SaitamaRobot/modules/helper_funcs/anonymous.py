dd3b6b40795c395547e7c88ba0d78905d7d629be
itzzzyashu <83384892+itzzzzyashu@users.noreply.github.com>
2021-12-27 00:47:15 +0530

    Update anonymous.py

--- SaitamaRobot/modules/helper_funcs/anonymous.py
+++ SaitamaRobot/modules/helper_funcs/anonymous.py
@@ -6,7 +6,7 @@ from telegram.ext import CallbackContext
 from telegram.inline.inlinekeyboardbutton import InlineKeyboardButton
 from telegram.inline.inlinekeyboardmarkup import InlineKeyboardMarkup
 
-from SaitamaRobot import DEV_USERS, SUDO_USERS, dispatcher
+from SaitamaRobot import DEV_USERS, DRAGONS, dispatcher
 from .decorators import kigcallback
 
 
