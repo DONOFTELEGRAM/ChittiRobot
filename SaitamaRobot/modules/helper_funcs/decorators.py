f498f0dd8e7ce98880fc49f6ffd9dd254f5872c9
itzzzyashu <83384892+itzzzzyashu@users.noreply.github.com>
2021-12-27 16:46:28 +0530

    Update and rename decorators.txt to decorators.py

--- SaitamaRobot/modules/helper_funcs/decorators.py
+++ SaitamaRobot/modules/helper_funcs/decorators.py
@@ -8,31 +8,32 @@ from typing import Optional, Union, List
 log = logging.getLogger('[Enterprise]')
 
 class KigyoTelegramHandler:
+    @run_async
     def __init__(self, d):
         self._dispatcher = d
-
+    @run_async
     def command(
             self, command: str, filters: Optional[BaseFilter] = None, admin_ok: bool = False, pass_args: bool = False,
             pass_chat_data: bool = False, run_async: bool = True, can_disable: bool = True,
             group: Optional[int] = 40
     ):
-
+        @run_async
         def _command(func):
             try:
                 if can_disable:
                     self._dispatcher.add_handler(
-                        DisableAbleCommandHandler(command, func, filters=filters, run_async=run_async,
+                        DisableAbleCommandHandler(command, func, filters=filters,
                                                   pass_args=pass_args, admin_ok=admin_ok), group
                     )
                 else:
                     self._dispatcher.add_handler(
-                        CommandHandler(command, func, filters=filters, run_async=run_async, pass_args=pass_args), group
+                        CommandHandler(command, func, filters=filters, pass_args=pass_args), group
                     )
                 log.debug(f"[KIGCMD] Loaded handler {command} for function {func.__name__} in group {group}")
             except TypeError:
                 if can_disable:
                     self._dispatcher.add_handler(
-                        DisableAbleCommandHandler(command, func, filters=filters, run_async=run_async,
+                        DisableAbleCommandHandler(command, func, filters=filters,
                                                   pass_args=pass_args, admin_ok=admin_ok, pass_chat_data=pass_chat_data)
                     )
                 else:
@@ -45,18 +46,18 @@ class KigyoTelegramHandler:
             return func
 
         return _command
-
+    @run_async
     def message(self, pattern: Optional[BaseFilter] = None, can_disable: bool = True, run_async: bool = True,
                 group: Optional[int] = 60, friendly=None):
         def _message(func):
             try:
                 if can_disable:
                     self._dispatcher.add_handler(
-                        DisableAbleMessageHandler(pattern, func, friendly=friendly, run_async=run_async), group
+                        DisableAbleMessageHandler(pattern, func, friendly=friendly), group
                     )
                 else:
                     self._dispatcher.add_handler(
-                        MessageHandler(pattern, func, run_async=run_async), group
+                        MessageHandler(pattern, func), group
                     )
                 log.debug(f"[KIGMSG] Loaded filter pattern {pattern} for function {func.__name__} in group {group}")
             except TypeError:
@@ -73,16 +74,16 @@ class KigyoTelegramHandler:
             return func
 
         return _message
-
+    @run_async
     def callbackquery(self, pattern: str = None, run_async: bool = True):
         def _callbackquery(func):
-            self._dispatcher.add_handler(CallbackQueryHandler(pattern=pattern, callback=func, run_async=run_async))
+            self._dispatcher.add_handler(CallbackQueryHandler(pattern=pattern, callback=func))
             log.debug(f'[KIGCALLBACK] Loaded callbackquery handler with pattern {pattern} for function {func.__name__}')
             return func
 
         return _callbackquery
-
-    def inlinequery(self, pattern: Optional[str] = None, run_async: bool = True, pass_user_data: bool = True,
+    @run_async
+    def inlinequery(self, pattern: Optional[str] = None, pass_user_data: bool = True,
                     pass_chat_data: bool = True, chat_types: List[str] = None):
         def _inlinequery(func):
             self._dispatcher.add_handler(
