6c928d627101d03da91a9162ddbef5c4c26be917
itzzzyashu <83384892+itzzzzyashu@users.noreply.github.com>
2021-12-27 20:12:28 +0530

    Update logomaker.py

--- SaitamaRobot/modules/logomaker.py
+++ SaitamaRobot/modules/logomaker.py
@@ -19,13 +19,13 @@ async def lego(event):
  await event.reply('Creating your logo...wait!')
  try:
     text = event.pattern_match.group(1)
-    img = Image.open('./Cutiepii_Robot/utils/Logo/blackbg.jpg')
+    img = Image.open('./SaitamaRobot/utils/Logo/blackbg.jpg')
     draw = ImageDraw.Draw(img)
     image_widthz, image_heightz = img.size
     pointsize = 500
     fillcolor = "gold"
     shadowcolor = "blue"
-    font = ImageFont.truetype("./Cutiepii_Robot/utils/Logo/Chopsic.otf", 330)
+    font = ImageFont.truetype("./SaitamaRobot/utils/Logo/Chopsic.otf", 330)
     w, h = draw.textsize(text, font=font)
     h += int(h*0.21)
     image_width, image_height = img.size
@@ -57,13 +57,13 @@ async def lego(event):
  await event.reply('Creating your logo...wait!')
  try:
     text = event.pattern_match.group(1)
-    img = Image.open('./Cutiepii_Robot/utils/Logo/blackbg.jpg')
+    img = Image.open('./SaitamaRobot/utils/Logo/blackbg.jpg')
     draw = ImageDraw.Draw(img)
     image_widthz, image_heightz = img.size
     pointsize = 500
     fillcolor = "white"
     shadowcolor = "blue"
-    font = ImageFont.truetype("./Cutiepii_Robot/utils/Logo/Maghrib.ttf", 1000)
+    font = ImageFont.truetype("./SaitamaRobot/utils/Logo/Maghrib.ttf", 1000)
     w, h = draw.textsize(text, font=font)
     h += int(h*0.21)
     image_width, image_height = img.size
@@ -71,13 +71,13 @@ async def lego(event):
     x = (image_widthz-w)/2
     y= ((image_heightz-h)/2+6)
     draw.text((x, y), text, font=font, fill="white", stroke_width=0, stroke_fill="white")
-    fname2 = "LogoByCutiepii.png"
+    fname2 = "RajniiRobotLogo.png"
     img.save(fname2, "png")
-    await tbot.send_file(event.chat_id, fname2, caption="Made By @Cutiepii_Robot")
+    await tbot.send_file(event.chat_id, fname2, caption="Made By @RajniiRobot")
     if os.path.exists(fname2):
             os.remove(fname2)
  except Exception as e:
-   await event.reply(f'Error Report @Black_Knights_Union_Support, {e}')
+   await event.reply(f'Error Report @RajniSupport,\n`{e}`')
 
 file_help = os.path.basename(__file__)
 file_help = file_help.replace(".py", "")
