3d6a624339cc04c061dabc5ef4f5dc93c43420c8
itzzzyashu <83384892+itzzzzyashu@users.noreply.github.com>
2021-12-28 18:19:36 +0530

    Update zombies.py

--- SaitamaRobot/modules/zombies.py
+++ SaitamaRobot/modules/zombies.py
@@ -6,7 +6,7 @@ from telethon.errors import ChatAdminRequiredError, UserAdminInvalidError
 from telethon.tl.functions.channels import EditBannedRequest
 from telethon.tl.types import ChatBannedRights, ChannelParticipantsAdmins
 
-from Cutiepii_Robot import telethn, OWNER_ID, DEV_USERS, DRAGONS, DEMONS
+from SaitamaRobot import telethn, OWNER_ID, DEV_USERS, DRAGONS, DEMONS
 
 # =================== CONSTANT ===================
 
