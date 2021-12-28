ab39890ed61fac928f73067c93dd9b297e865d18
itzzzyashu <83384892+itzzzzyashu@users.noreply.github.com>
2021-12-29 04:15:47 +0530

    Update userinfo.py

--- SaitamaRobot/modules/userinfo.py
+++ SaitamaRobot/modules/userinfo.py
@@ -33,7 +33,7 @@ from SaitamaRobot.modules.sql.users_sql import get_user_num_chats
 from SaitamaRobot.modules.helper_funcs.chat_status import sudo_plus
 from SaitamaRobot.modules.helper_funcs.extraction import extract_user
 from SaitamaRobot import telethn as SaitamaTelethonClient
-from SaitamaRobot.modules.arq import statistics
+from SaitamaRobot.modules.arq import arq_stats
 # from SaitamaRobot.modules.arq.arq_stats import statistics
 
 def no_by_per(totalhp, percentage):
