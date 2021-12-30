67b52de93fc5d6a976e79ea8318e7e032fc3d4a3
itzzzyashu <83384892+itzzzzyashu@users.noreply.github.com>
2021-12-30 20:55:04 +0530

    Update markdown.py

--- SaitamaRobot/modules/markdown.py
+++ SaitamaRobot/modules/markdown.py
@@ -4,7 +4,7 @@ Markdown is a very powerful formatting tool supported by telegram. @RajniiRobot 
 saved messages are correctly parsed, and to allow you to create buttons.
 • italic: wrapping text with 2 undersore will produce italic text.
 • bold: wrapping text with Asterisk will produce bold text.
-• code or monospace: wrapping text with '`' will produce monospaced text, also known as code.
+• code or monospace: wrapping text with acute or 'left quote' will produce monospaced text, also known as code.
 • Inline Button & link text:
 Contact @RajniSupport to know about how to make
  Inline Url button or link text.
