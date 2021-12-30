ea153fe9df9a4984cab2bc8dadab69402962866b
itzzzyashu <83384892+itzzzzyashu@users.noreply.github.com>
2021-12-31 01:04:40 +0530

    Update __main__.py

--- SaitamaRobot/__main__.py
+++ SaitamaRobot/__main__.py
@@ -302,7 +302,7 @@ def start(update: Update, context: CallbackContext):
 
         else:
             update.effective_message.reply_video(
-                BOT_PIC,
+                caption=BOT_PIC,
                 PM_START_TEXT,
                 reply_markup=InlineKeyboardMarkup(buttons),
                 parse_mode=ParseMode.MARKDOWN,
