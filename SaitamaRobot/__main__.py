ea69b8b101e24f84700269249e9f3fb259ab6f1e
itzzzyashu <83384892+itzzzzyashu@users.noreply.github.com>
2021-12-31 22:39:14 +0530

    Update __main__.py

--- SaitamaRobot/__main__.py
+++ SaitamaRobot/__main__.py
@@ -795,7 +795,7 @@ def main():
 
     if SUPPORT_CHAT is not None and isinstance(SUPPORT_CHAT, str):
         try:
-            dispatcher.bot.sendMessage(f"@ERROR_LOGS", "I am now online!")
+            dispatcher.bot.sendMessage(f"@{ERROR_LOGS}", "I am now online!")
         except Unauthorized:
             LOGGER.warning(
                 "Bot isnt able to send message to ERROR LOG CHANNEL chat, go and check!")
