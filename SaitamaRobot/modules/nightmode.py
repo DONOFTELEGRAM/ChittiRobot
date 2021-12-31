d868086aa690eaf981fcce02e70d0362531a9df3
itzzzyashu <83384892+itzzzzyashu@users.noreply.github.com>
2021-12-31 15:42:23 +0530

    Update nightmode.py

--- SaitamaRobot/modules/nightmode.py
+++ SaitamaRobot/modules/nightmode.py
@@ -1,4 +1,3 @@
-
 import os
 
 from telethon.tl.types import ChatBannedRights
@@ -130,7 +129,7 @@ async def job_close():
     for pro in chats:
         try:
             await telethn.send_message(
-              int(pro.chat_id), "12:00 Am, Group Is Closing Till 6 Am. Night Mode Started ! \n**Powered By Yūki Network**"
+              int(pro.chat_id), "12:00 AM, Group Is Closing Till 6:00 Am. Night Mode Started ! \n**Powered By @SanatanRakshaNetwork**"
             )
             await telethn(
             functions.messages.EditChatDefaultBannedRightsRequest(
@@ -152,7 +151,7 @@ async def job_open():
     for pro in chats:
         try:
             await telethn.send_message(
-              int(pro.chat_id), "06:00 Am, Group Is Opening.\n**Powered By Yūki Network**"
+              int(pro.chat_id), "06:00 AM, Group is Opened till 12:AM tommorow. Night Mode stopped !\n**@SanatanRakshaNetwork**"
             )
             await telethn(
             functions.messages.EditChatDefaultBannedRightsRequest(
@@ -169,10 +168,10 @@ scheduler.start()
 
 
 __help__ = """
-  ➢ `/nightmode`*:* on/off
+• `/nightmode on/off`**:** toggle nightmode Enable or not.
  
-**Note:** Night Mode chats get Automatically closed at 12pm(IST)
-and Automatically openned at 6am(IST) To Prevent Night Spams.
+**Note:** Night Mode chats get Automatically closed at 12 AM (IST)
+and Automatically openned at 6 AM (IST) To Prevent Night Spams.
 """
 
 __mod_name__ = "NightMode"
