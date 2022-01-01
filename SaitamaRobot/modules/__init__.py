031f1bc956fc936e22517dfe5ef85463df2c2f5f
itzzzyashu <83384892+itzzzzyashu@users.noreply.github.com>
2022-01-01 11:46:17 +0530

    Update __init__.py

--- SaitamaRobot/modules/__init__.py
+++ SaitamaRobot/modules/__init__.py
@@ -1,4 +1,7 @@
+
+import sys
 from SaitamaRobot import LOAD, LOGGER, NO_LOAD
+
 
 def __list_all_modules():
     import glob
@@ -9,18 +12,18 @@ def __list_all_modules():
     all_modules = [
         basename(f)[:-3]
         for f in mod_paths
-        if isfile(f) and f.endswith(".py") and not f.endswith('__init__.py')
+        if isfile(f) and f.endswith(".py") and not f.endswith("__init__.py")
     ]
 
     if LOAD or NO_LOAD:
         to_load = LOAD
         if to_load:
             if not all(
-                    any(mod == module_name
-                        for module_name in all_modules)
-                    for mod in to_load):
+                any(mod == module_name for module_name in all_modules)
+                for mod in to_load
+            ):
                 LOGGER.error("Invalid loadorder names. Quitting.")
-                quit(1)
+                sys.exit(1)
 
             all_modules = sorted(set(all_modules) - set(to_load))
             to_load = list(all_modules) + to_load
@@ -40,46 +43,3 @@ def __list_all_modules():
 ALL_MODULES = __list_all_modules()
 LOGGER.info("Modules to load: %s", str(ALL_MODULES))
 __all__ = ALL_MODULES + ["ALL_MODULES"]
-
-
-
-"""
-MIT License
-Copyright (C) 2017-2019, Paul Larsen
-Copyright (C) 2021 Awesome-RJ
-Copyright (c) 2021, Yūki • Black Knights Union, <https://github.com/Awesome-RJ/CutiepiiRobot>
-This file is part of @Cutiepii_Robot (Telegram Bot)
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-"""
-
-from SaitamaRobot import DB_URL
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import scoped_session, sessionmaker
-
-
-def start() -> scoped_session:
-    engine = create_engine(DB_URL, client_encoding="utf8")
-    BASE.metadata.bind = engine
-    BASE.metadata.create_all(engine)
-    return scoped_session(sessionmaker(bind=engine, autoflush=False))
-
-
-BASE = declarative_base()
-SESSION = start()
-
-
