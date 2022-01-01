b21ae8bf2eedd32c794d4fc12628634cae11b195
itzzzyashu <83384892+itzzzzyashu@users.noreply.github.com>
2022-01-01 09:12:47 +0530

    Update warns_sql.py

--- SaitamaRobot/modules/sql/warns_sql.py
+++ SaitamaRobot/modules/sql/warns_sql.py
@@ -9,7 +9,6 @@ from sqlalchemy.dialects import postgresql
 
 class Warns(BASE):
     __tablename__ = "warns"
-
     user_id = Column(BigInteger, primary_key=True)
     chat_id = Column(String(14), primary_key=True)
     num_warns = Column(Integer, default=0)
