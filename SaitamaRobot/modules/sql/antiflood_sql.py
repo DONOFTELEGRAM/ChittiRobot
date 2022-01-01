e179548a81660ee02e1dc450d45fb5b8d2b0bf94
itzzzyashu <83384892+itzzzzyashu@users.noreply.github.com>
2022-01-01 08:52:59 +0530

    Update antiflood_sql.py

--- SaitamaRobot/modules/sql/antiflood_sql.py
+++ SaitamaRobot/modules/sql/antiflood_sql.py
@@ -1,7 +1,7 @@
 import threading
 
-from sqlalchemy import String, Column, Integer, UnicodeText
-from sqlalchemy.sql.sqltypes import BigInteger
+from sqlalchemy import String, Column, Integer, UnicodeText, BigInteger
+
 from SaitamaRobot.modules.sql import SESSION, BASE
 DEF_COUNT = 1
 DEF_LIMIT = 0
