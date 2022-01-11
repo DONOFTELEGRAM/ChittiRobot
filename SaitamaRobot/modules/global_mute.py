37acfc49a970237a2e816679e3f3203f309cd165
itzzzyashu <83384892+itzzzzyashu@users.noreply.github.com>
2022-01-12 00:17:44 +0530

    Update global_mute.py

--- SaitamaRobot/modules/global_mute.py
+++ SaitamaRobot/modules/global_mute.py
@@ -130,7 +130,8 @@ def gmute(update: Update, context: CallbackContext):
             continue
 
         try:
-            if user.can_send_messages is None or user.can_send_messages:
+            member = chat.get_member(user_id)
+            if member.can_send_messages is None or member.can_send_messages:
                 chat_permissions = ChatPermissions(can_send_messages=False)
                 bot.restrict_chat_member(chat_id, user_id, chat_permissions)
                 gmuted_chats += 1
